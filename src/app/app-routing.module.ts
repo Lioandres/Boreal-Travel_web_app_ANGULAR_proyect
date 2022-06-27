4ebebc848739cf7f75fc5e40b751a2ef983ea1ab
unknown <formacio@PC-1062606.formacio.local>
2022-06-27 13:09:46 +0200

    from ITAc to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ExcursionsComponent } from './excursions/excursions.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path:'',
+  component:AppComponent,
+  pathMatch:'full'
+},
+
+{
+  path:'home',
+  component:HomeComponent,
+  pathMatch:'full'
+},
+
+{
+  path:'excursionsList',
+  component:ExcursionsComponent,
+  pathMatch:'full'
+},
+
+];
 
 @NgModule({
+ 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Spring_9_Tasca';
+  started:boolean=false
 }

c02b3f4bfb31649dab6c527b24117b07973db64a
MAC <MAC@MacBook-Emilio.local>
2022-08-16 15:05:27 +0200

    put request in Angular

--- src/app/services/petition.service.ts
+++ src/app/services/petition.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Show } from '../interfaces/show.interface';
 import { ExcursionTemplate, TemplateList } from '../interfaces/templateList.interface';
+import { ExcursionsService } from './excursions.service';
 
 @Injectable({
   providedIn: 'root'
@@ -48,6 +49,62 @@ export class PetitionService {
       })
 }
 
+
+modifyExcursionTemp() {
+  this.http.put<Show>('http://localhost:8080/api/template/update/20',
+  {
+    "title":"sss",
+     "img":"dddd",
+     "type":"wwww",
+     "description":"sssss",
+     "price_default":1.20,
+     "max_num_people_default":30
+})
+            .subscribe((resp:Show)=>{
+                  this.respShow=resp
+                  console.log(this.respShow.data)
+                             
+     })
+}
+
+// modifyExcursionTemp(id:number,title:string,img:string,type:string,description:string,price_default:number,max_num_people_default:number) {
+//   this.http.put<Show>('http://localhost:8080/api/template/update/'+id,
+//   {
+//     "title":title,
+//      "img":img,
+//      "type":type,
+//      "description":description,
+//      "price_default":price_default,
+//      "max_num_people_default":max_num_people_default
+// })
+//             .subscribe((resp:Show)=>{
+//                   this.respShow=resp
+//                   console.log(this.respShow.data)
+                             
+//      })
+// }
+
+// modifyExcursionTemp(excursionData:ExcursionTemplate) {
+//   this.http.put<Show>('http://localhost:8080/api/template/update/'+excursionData.id_excursion_template,{
+//     "title":excursionData.title,
+//      "img":excursionData.img,
+//      "type":excursionData.type,
+//      "description":excursionData.description,
+//      "price_default":excursionData.price_default,
+//      "max_num_people_default":excursionData.max_num_people_default
+// })
+//             .subscribe((resp:Show)=>{
+//                   this.respShow=resp
+//                   console.log(this.respShow.data)
+                             
+//      })
+// }
+
+
  
 
 }
+function body<T>(arg0: string, body: any, arg2: { title: any; img: any; description: any; type: any; price: any; }) {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/templates/modify-temp/modify-temp.component.ts
+++ src/app/templates/modify-temp/modify-temp.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { PetitionService } from 'src/app/services/petition.service';
 
 @Component({
   selector: 'app-modify-temp',
@@ -8,24 +9,58 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 })
 export class ModifyTempComponent implements OnInit {
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,
+    private petitionServ:PetitionService) { }
 
   ngOnInit(): void {
   }
 
   modifyTempForm:FormGroup= this.fb.group({
-    id:["",[Validators.required,Validators.pattern("[0-9]{1,3}")]],
-    title:["",[Validators.required,Validators.maxLength(10)]],
-    path:["",[Validators.required]],
-    // type:["",[Validators.required]],
-    // description:["",[Validators.required]],
-    // price:["",[Validators.required]],
-    // people:["",[Validators.required]],
+    id_excursion_template:["",[Validators.required,Validators.pattern("[0-9]{1,3}")]],
+    title:["",[Validators.required, Validators.maxLength(30)]],
+    img:["",[Validators.required]],
+    type:["",[Validators.required]],
+    description:["",[Validators.required,Validators.maxLength(1000)]],
+    price_default:["",[Validators.required, Validators.pattern("[0-9]{1,4}[.][0-9]{2}")]],
+    max_num_people_default:["",[Validators.required,Validators.max(30)]],
 
   });
 
+  modifyExcursionTemp(){
+    if(this.modifyTempForm.valid){
+
+      console.log(this.modifyTempForm.value)
+
+      console.log(
+      this.modifyTempForm.get('id')?.value,
+      this.modifyTempForm.get('title')?.value,
+      this.modifyTempForm.get('img')?.value,
+      this.modifyTempForm.get('type')?.value,
+      this.modifyTempForm.get('description')?.value,
+      this.modifyTempForm.get('price_default')?.value,
+      this.modifyTempForm.get('max_num_people_default')?.value)
+
+      this.petitionServ.modifyExcursionTemp(
+        // this.modifyTempForm.get('id')?.value,
+        // this.modifyTempForm.get('title')?.value,
+        // this.modifyTempForm.get('img')?.value,
+        // this.modifyTempForm.get('type')?.value,
+        // this.modifyTempForm.get('description')?.value,
+        // this.modifyTempForm.get('price_default')?.value,
+        // this.modifyTempForm.get('max_num_people_default')?.value
+      ) 
+    }
+  }
+
+  // modifyExcursionTemp(){
+  //   if(this.modifyTempForm.valid){
+  //     console.log(this.modifyTempForm.value)
+  //     this.petitionServ.modifyExcursionTemp(this.modifyTempForm.value) 
+  //   }
+  // }
+
   submit(){
-    console.log(this.modifyTempForm.get('title'))
+    console.log(this.modifyTempForm.value)
   }
 
 }
